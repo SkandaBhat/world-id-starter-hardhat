8db4a0b6f48d421e4f1d5972dd5fc878db270df7
SkandaBhat <bhat.skanda.m@gmaill.com>
2022-12-03 16:30:16 +0530

    worldcoin poc

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,5 @@
 import { ethers } from 'hardhat'
+const hre = require('hardhat');
 
 async function main() {
     const worldIDAddress = await fetch('https://developer.worldcoin.org/api/v1/contracts')
@@ -6,9 +7,20 @@ async function main() {
         .then(res => res.find(({ key }) => key === 'staging.semaphore.wld.eth').value)
 
     const ContractFactory = await ethers.getContractFactory('Contract')
-    const contract = await ContractFactory.deploy(worldIDAddress)
+    const contract = await ContractFactory.deploy(worldIDAddress, "wid_staging_f76caada4a091ea4b8423fa667be9f07")
 
     await contract.deployed()
+
+    const config = {
+        address: contract.address,
+        constructorArguments: [worldIDAddress, "wid_staging_f76caada4a091ea4b8423fa667be9f07"],
+    };
+
+    try {
+        await hre.run('verify:verify', config);
+    } catch (err) {
+        console.error('Etherscan verification failed', err);
+    }
 
     console.log('Contract deployed to:', contract.address)
 }
